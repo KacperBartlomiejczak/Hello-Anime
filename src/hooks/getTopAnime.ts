ea83fcf694d9571ae23ade50c3e1b0ffe61261a6
Kacperbartlomiejczak <kacperbartlomiejczak@proton.me>
2025-12-20 11:54:20 +0100

    Changes in getTopAnime.ts

--- src/hooks/getTopAnime.ts
+++ src/hooks/getTopAnime.ts
@@ -2,17 +2,16 @@ export async function getTopAnime(isUpcomming: boolean) {
   const url = "https://api.jikan.moe/v4";
 
   let endpoint = "/top/anime";
-  let params = new URLSearchParams({
-    sfw: "true",
-    type: "tv",
-    limit: "10",
-    filter: isUpcomming ? "upcoming" : "bypopularity",
-  });
 
   try {
-    const response = await fetch(`${url}${endpoint}?${params.toString()}`, {
-      next: { revalidate: 3600 },
-    });
+    const response = await fetch(
+      `${url}${endpoint}?sfw=true&type=tv&limit=10&${
+        isUpcomming ? "filter=upcoming" : "filter=bypopularity"
+      }`,
+      {
+        next: { revalidate: 3600 },
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Couldn't connected with api ${response.status}`);
