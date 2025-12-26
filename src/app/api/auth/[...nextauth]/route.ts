6aeec257a7d9b3576e7a3a23db794d7f44dc7943
Kacperbartlomiejczak <kacperbartlomiejczak@proton.me>
2025-12-26 13:18:37 +0100

    Add authentication support with NextAuth; implement auth provider and configure GitHub and Google providers

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import { AuthOptions } from "next-auth";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string
+    })
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST}
