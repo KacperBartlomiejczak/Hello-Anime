1d36eaad56e073854b8caae25a5edbe60a78d5fe
Kacper Bartlomiejczak <kacperbartlomiejczak@proton.me>
2026-03-02 10:28:02 +0100

    feat: Implement NextAuth authentication with Google and GitHub providers using Drizzle adapter.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,9 +4,14 @@ import GithubProvider from "next-auth/providers/github";
 import { AuthOptions } from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db";
+import { users, sessions, accounts } from "@/db/schema";
 
 export const authOptions: AuthOptions = {
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    sessionsTable: sessions,
+    accountsTable: accounts,
+  }),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -22,20 +27,20 @@ export const authOptions: AuthOptions = {
     signIn: "/login",
   },
   callbacks: {
-    async jwt({token, user}){
-      if(user){
-        token.sub = user.id
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
       }
-      return token
+      return token;
     },
-    async session({session, token}){
-      if (session.user && token.sub){
-          session.user.id = token.sub
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
       }
-      
-      return session
-    }
-  }
+
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
